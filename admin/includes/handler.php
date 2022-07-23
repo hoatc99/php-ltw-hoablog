01a8bb11bef853ba30a7a873feadb98a713e1b64
hoatc99 <camhoa.tran99@gmail.com>
2022-07-24 00:38:53 +0700

    Fix some bugs

--- admin/includes/handler.php
+++ admin/includes/handler.php
@@ -3,10 +3,11 @@
     function redirect($url = null) {
         if ($url == null) {
             header('Location: http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+            exit;
         } else {
             header('Location: ' . $url);
+            exit;
         }
-        exit;
     }
 
     function flag_get($flag_name = 'flag') {
