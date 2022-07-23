2b07b2ebf6484f72ab966bd7abacd0a0c3d16878
hoatc99 <camhoa.tran99@gmail.com>
2022-07-24 04:20:25 +0700

    Fix redirect bugs

--- admin/includes/handler.php
+++ admin/includes/handler.php
@@ -3,13 +3,13 @@
     session_start();
 
     function redirect($url) {
-        // if ($url == null) {
-        //     header('Location: http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
-        // } else {
-        //     header('Location: ' . $url);
-        // }
-        header('Location: ' . $url);
-        exit();
+        if ($url == null) {
+            header('Location: http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+            exit();
+        } else {
+            header('Location: ' . $url);
+            exit();
+        }
     }
 
     function flag_get($flag_name = 'flag') {
